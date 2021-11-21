5fcdcc37eebe889f3c59661eac94237eb0bf311e
evgenkolesman <glavstroi_e@mail.ru>
2021-11-21 21:52:53 +0300

    Начинаю переводить в микросервисы остатки докинул

---
+++ role/src/test/java/ru/koleson/role/RoleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.koleson.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
